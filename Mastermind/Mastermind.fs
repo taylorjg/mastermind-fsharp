71fac1139550720f77a233254b39d56d29185436
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-17 13:01:58 +0100

    Implement evaluateGuess

---
+++ Mastermind/Mastermind.fs
@@ -0,0 +1,19 @@
+module Mastermind
+
+type Peg = R | G | B | Y | BL | WH
+type Code = { p0: Peg; p1: Peg; p2: Peg; p3: Peg }
+type Score = { blacks: int; whites: int }
+
+let allPegs = [R; G; B; Y; BL; WH]
+
+let pegs code = [code.p0; code.p1; code.p2; code.p3]
+
+let evaluateGuess secret guess =
+    let spegs = pegs secret
+    let gpegs = pegs guess
+    let countMatchingPegs p1 pegs = List.filter (fun p2 -> p1 = p2) pegs |> List.length
+    let minPegs p = min (countMatchingPegs p spegs) (countMatchingPegs p gpegs)
+    let sumOfMins = List.sumBy minPegs allPegs
+    let blacks = List.zip spegs gpegs |> List.filter (fun (a, b) -> a = b) |> List.length
+    let whites = sumOfMins - blacks
+    { blacks = blacks; whites = whites }
---
+++ Tests/EvaluateGuess.fs
@@ -0,0 +1,30 @@
+module Tests
+
+open Expecto
+open Mastermind
+
+[<Tests>]
+let tests =
+  testList "evaluateGuess" [
+
+    testCase "no overlap" <| fun _ ->
+      let secret = { p0 = R; p1 = R; p2 = G; p3 = G }
+      let guess = { p0 = BL; p1 = BL; p2 = WH; p3 = WH }
+      let score = evaluateGuess secret guess
+      Expect.equal score.blacks 0 "score.blacks"
+      Expect.equal score.whites 0 "score.whites"
+
+    testCase "exact match" <| fun _ ->
+      let secret = { p0 = R; p1 = R; p2 = G; p3 = G }
+      let guess = { p0 = R; p1 = R; p2 = G; p3 = G }
+      let score = evaluateGuess secret guess
+      Expect.equal score.blacks 4 "score.blacks"
+      Expect.equal score.whites 0 "score.whites"
+
+    testCase "correct colours in wrong positions" <| fun _ ->
+      let secret = { p0 = R; p1 = G; p2 = B; p3 = Y }
+      let guess = { p0 = Y; p1 = B; p2 = G; p3 = R }
+      let score = evaluateGuess secret guess
+      Expect.equal score.blacks 0 "score.blacks"
+      Expect.equal score.whites 4 "score.whites"
+  ]
