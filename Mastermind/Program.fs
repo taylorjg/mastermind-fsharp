2fdd395fa3d86bcb36b5e0b274f268f393c0d856
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-17 22:32:19 +0100

    Update main to demo autosolve

--- Mastermind/Program.fs
+++ Mastermind/Program.fs
@@ -1,6 +1,11 @@
-﻿open System
+﻿open Mastermind
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let secret = { p0 = G; p1 = B; p2 = BL; p3 = WH }
+    printfn "secret: %A" secret
+    let guesses = autosolve (evaluateGuess secret)
+    printfn "Number of guesses: %d" <| List.length guesses
+    let printGuess pair = printfn "guess: %A score: %A" (fst pair) (snd pair)
+    List.map printGuess guesses |> ignore
+    0
